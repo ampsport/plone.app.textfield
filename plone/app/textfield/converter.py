f9353f9f26fb52e0ffb5f8d5ed48aeea19902773
JMehring <JMehring@localhost>
2010-09-27 06:20:37 +0000

    added new IWigetInputConverter Code
    svn path=/plone.app.textfield/branches/jmehring-drafts/; revision=40317

---
+++ plone/app/textfield/converter.py
@@ -0,0 +1,18 @@
+import zope.component
+
+from plone.z3cform.converter import DefaultWidgetInputConverter
+
+import z3c.form.interfaces
+
+from plone.app.textfield.interfaces import IRichText
+from plone.app.textfield.widget import IRichTextWidget
+
+
+class RichTextWidgetInputConverter(DefaultWidgetInputConverter):
+    """Converts rich text object into a suitable form imput value"""
+
+    zope.component.adapts(IRichText, IRichTextWidget)
+
+    def toWidgetInputValue(self, value):
+        """See interfaces.IWidgetInputConverter"""
+        return z3c.form.interfaces.IDataConverter(self.widget).toFieldValue(value).raw
